e1f2eef9593ca380625d2e703133c9ee04460e7a
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-02-12 16:56:16 -0500

    Fix merge sort algorithm to correctly merge right array

--- merge_sort.py
+++ merge_sort.py
@@ -26,4 +26,6 @@ def merge_sort(array):
         sorted_index += 1
 
     while right_array_index < len(right_part):
-        pass
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
